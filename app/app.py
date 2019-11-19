cd9fcd410875b4850275868d87ea153ca60759af
Mahesh Samaradiwakara <hesh.here@gmail.com>
2019-11-19 12:06:17 +1100

    Update graphs on mouseOver each tile

--- app/app.py
+++ app/app.py
@@ -5,26 +5,24 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
-
 import json
-file = open("../data/phase-02/phase-02.geojson")
-counties = json.load(file)
 import pandas as pd
-df = pd.read_csv("../data/phase-02/m.csv", dtype={"id": str})
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+
+file = open("../data/phase-02/phase-02.geojson")
+counties = json.load(file)
+df = pd.read_csv("../data/phase-02/m.csv", dtype={"id": str})
 
 mapbox_token = 'pk.eyJ1IjoibWFoZXNoeCIsImEiOiJjazJsNnlwcmQwNDFjM2xtdWNoM244emU4In0.YBO8xPG8Iw-BfL8sxSkpTw'
 
 app = dash.Dash('sdthon')
 
-text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
-
-fig1 = go.Figure(go.Choroplethmapbox(geojson=counties, locations=df.id, z=df.harvested,
+fig1 = go.Figure(go.Choroplethmapbox(geojson=counties,  locations=df.id, z=df.harvested,
                                     colorscale="Viridis", zmin=0, zmax=166448,
                                     marker_opacity=0.5, marker_line_width=0))
 
-fig1.update_layout(mapbox_style="carto-positron", mapbox_accesstoken=mapbox_token,  mapbox_zoom=8,
+fig1.update_layout(mapbox_style="carto-positron", mapbox_accesstoken=mapbox_token,  mapbox_zoom=7,
                 mapbox_center = {"lat": -20.3168, "lon": 148.356})
 #
 # fig1.show()
@@ -61,71 +59,40 @@ fig.update_yaxes(title_text="<b>harvested</b>", secondary_y=True)
 fig2 = dict(data=[dict(type='bar', x=df['date'], y=df['harvested'])])
 
 app.layout = html.Div([
-     html.Div([
-        html.H2('satellite-datathon', style=text_style),
-        html.P([
-                html.Label("Choose a Tile"),
-                dcc.Dropdown(id = 'opt', options = opts,value = '1')],
-                style = {'width': '400px', 'fontSize' : '20px','padding-left' : '100px', 'display': 'inline-block'}),
         dcc.Graph(id='plot1', figure=fig1),
-        html.Div(id='text1'),
-     ], style={'width': '49%', 'display': 'inline-block'}),
-     html.Div([
         dcc.Graph(id = 'plot', figure = fig),
-     ], style= {'width': '49%', 'display': 'inline-block'})
+
+        html.Div([
+        dcc.Dropdown(id = 'opt', options = opts, value = '1'),
+        html.Div(id='text1'),
+        ], style={'display': 'none'}),
+
     ])
 
 @app.callback(Output('plot', 'figure'),
              [Input('opt', 'value')])
 def update_figure(value):
     fig.data = []
-    # filtering the data
-    #st2 = st[(st.Date > dates[input2[0]]) & (st.Date < dates[input2[1]])]
     st2 = df[(df.tile == value)]
-    # updating the plot
     trace_1 = go.Scatter(x = st2.Date, y = st2['ndvi'],
-                        name = 'ndvi',
-                        line = dict(width = 2,
-                                    color = 'rgb(255, 0, 0)'))
+                        name = 'ndvi', line = dict(width = 2, color = 'rgb(255, 0, 0)'))
     trace_2 = go.Scatter(x = st2.Date, y = st2['cc_p80_perc'],
-                        name = 'cc',
-                        line = dict(width = 2,
-                                    color = 'rgb(0, 255, 0)'))
+                        name = 'cc', line = dict(width = 2, color = 'rgb(0, 255, 0)'))
     trace_3 = go.Scatter(x = st2.Date, y = st2['harvested'],
-                        name = 'harvested',
-                        line = dict(width = 2,
-                                    color = 'rgb(0, 0, 255)'))
-    #fig = go.Figure(data = [trace_1, trace_2, trace_3], layout = layout)
-
-    # Add traces
-    fig.add_trace(
-        go.Scatter(trace_1),
-        secondary_y=False,
-    )
-
-    # Add traces
-    fig.add_trace(
-        go.Scatter(trace_2),
-        secondary_y=False,
-    )
-
-    fig.add_trace(
-        go.Scatter(trace_3),
-        secondary_y=True,
-    )
-
+                        name = 'harvested', line = dict(width = 2,color = 'rgb(0, 0, 255)'))
+    fig.add_trace(go.Scatter(trace_1),secondary_y=False,)
+    fig.add_trace(go.Scatter(trace_2),secondary_y=False,)
+    fig.add_trace(go.Scatter(trace_3),secondary_y=True,)
     return fig
 
-
-
-
-
-@app.callback(Output('text1', 'children'), [Input('plot1', 'selectedData')] )
-def text_callback(selectedData):
-    if selectedData is None:
+@app.callback(Output('opt', 'value'), [Input('plot1', 'hoverData')] )
+def text_callback(hoverData):
+    if hoverData is None:
         return 'Empty'
     else:
-        return selectedData
+        x = hoverData["points"][0]["location"]
+        y = df.loc[df['id'] == x, 'tile'].iloc[0]
+        return y #hoverData["points"][0]["pointNumber"]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
